ec94bc226b3142469969656b0b40a9be386d2ee2
ljx <jingxiang.lang@xiamalaya.com>
2026-04-27 17:02:33 +0800

    1

--- runner/train.py
+++ runner/train.py
@@ -693,10 +693,11 @@ class AF3Trainer(object):
                 self.train_step(batch)
                 if use_ema and is_update_step:
                     self.ema_wrapper.update()
-                print(f"---------------{DIST_WRAPPER.rank}:{self.step}-----------------", flush=True)
+                print(f"---------------{DIST_WRAPPER.rank}{self.step}-----------------", flush=True)
                 if step_need_log or is_last_step:
                     metrics = self.train_metric_wrapper.calc()
                     self.print(f"Step {self.step} train metrics: {metrics}")
+                    print(f"Step {self.step} train metrics: {metrics}", flush=True)
                     last_lr = self.lr_scheduler.get_last_lr()
                     if DIST_WRAPPER.rank == 0:
                         if self.configs.use_wandb:
